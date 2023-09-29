4713a2c70528ba6ba26a3c720874c2c613e661a7
HardcoreMagazine <HardcoreMagazine@users.noreply.github.com>
2023-09-29 12:16:16 +0600

    Functional GET, POST (create) requests, logging for SqlFactory.

--- Controllers/Posts.cs
+++ Controllers/Posts.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
 using SelenicSparkApp_v2_WebAPI.Models;
 using SelenicSparkApp_v2_WebAPI.Utilities;
-using System.IO.Pipelines;
 
 namespace SelenicSparkApp_v2_WebAPI.Controllers
 {
@@ -14,6 +13,22 @@ namespace SelenicSparkApp_v2_WebAPI.Controllers
         private readonly MySqlConnection _connection;
         private SqlFactory _sqlFactory;
 
+        private const string PostsTable = "selenicspark.posts";
+
+        private const int MinPostTitleLen = 4;
+        private const int MaxPostTitleLen = 300;
+        private const int MaxPostTextLen = 20_000;
+
+/*
+        private const int MinCommentLen = 3;
+        private const int MaxCommentLen = 3000;
+
+        private const int PostsPerPage = 25; // Default: 25
+        private const int PostPreviewTextLen = 450;
+
+        private const int MaxSearchPhraseLen = 64;
+*/
+
         public Posts(ILogger<Posts> logger, MySqlConnection connection)
         {
             _logger = logger;
@@ -24,21 +39,49 @@ namespace SelenicSparkApp_v2_WebAPI.Controllers
         [HttpGet(Name = "Posts")]
         public async Task<IEnumerable<Post>?> GetAsync()
         {
-            string query = "SELECT * FROM selenicspark.posts;";
+            string query = $"SELECT * FROM {PostsTable};";
             Func<MySqlDataReader, Post> mapFunction = reader =>
             {
                 return new Post
                 {
-                    // TODO: fix this mess
-                    // Exception thrown: 'System.InvalidCastException' in System.Private.CoreLib.dll
                     PostID = (int)reader.GetValue(0),
                     Title = (string)reader.GetValue(1),
-                    Text = (string)reader.GetValue(2),
+                    Text = reader.GetValue(2).ToString(),
                     Author = (string)reader.GetValue(3),
-                    Created = DateTimeOffset.Parse((string)reader.GetValue(4))
+                    Created = (DateTime)reader.GetValue(4)
                 };
             };
             return await _sqlFactory.Select(query, mapFunction);
+        }
+
+        [HttpPost(Name = "Posts")]
+        public async Task<bool> PostAsync(Post post)
+        {
+            if (string.IsNullOrWhiteSpace(post.Title) || string.IsNullOrWhiteSpace(post.Author))
+            {
+                return false;
+            }
+
+            if (post.Title.Length < MinPostTitleLen)
+            {
+                return false;
+            }
+
+            if (post.Title.Length > MaxPostTitleLen)
+            {
+                post.Title = post.Title[0..MaxPostTitleLen];
+            }
+
+            if (!string.IsNullOrWhiteSpace(post.Text) && post.Text.Length > MaxPostTextLen)
+            {
+                post.Text = post.Text[0..MaxPostTextLen];
+            }
+
+            string query = 
+                $"INSERT INTO {PostsTable} (`Title`, `Text`, `Author`, `Created`) " +
+                $"VALUES ('{post.Title}', '{post.Text}', '{post.Author}', '{DateTimeOffset.UtcNow:yyyy.MM.dd HH:mm:ss}');";
+            
+            return await _sqlFactory.Insert(query);
         }
     }
 }
--- Utilities/SqlFactory.cs
+++ Utilities/SqlFactory.cs
@@ -4,18 +4,56 @@ using System.Data.Common;
 
 namespace SelenicSparkApp_v2_WebAPI.Utilities
 {
-    /// <summary>
-    /// Allows to shorten SELECT, UPDATE, DELETE database requests by using generics
-    /// </summary>
+    /// <summary>Allows to shorten CRUD actions with database using generics and abstraction</summary>
     public class SqlFactory
     {
         public readonly ILogger<ControllerBase> Logger;
         public readonly MySqlConnection Connection;
 
+        /// <summary>Allows to shorten CRUD actions with database using generics and abstraction</summary>
+        /// <param name="connection">Database connection instance</param>
+        /// <param name="logger">Logger, native to controller making database request</param>
         public SqlFactory(MySqlConnection connection, ILogger<ControllerBase> logger)
         {
             Connection = connection;
             Logger = logger;
+        }
+
+        public async Task<bool> Insert(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query) || !query.Contains("INSERT")) // Dummy failsafe
+            {
+                return false;
+            }
+            try
+            {
+                await Connection.OpenAsync();
+                var command = new MySqlCommand(query, Connection);
+                var reader = await command.ExecuteReaderAsync();
+                if (reader.RecordsAffected > 0)
+                {
+                    Logger.LogInformation($"Created new database entry, q='{query}'");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (DbException exc)
+            {
+                Logger.LogError($"Database error has occured: '{exc.Message}'");
+                return false;
+            }
+            catch (Exception exc)
+            {
+                Logger.LogWarning($"Unknown error has occured: '{exc.Message}'");
+                return false;
+            }
+            finally
+            {
+                await Connection.CloseAsync();
+            }
         }
 
         /// <summary>Selects the specified query.</summary>
@@ -35,7 +73,7 @@ namespace SelenicSparkApp_v2_WebAPI.Utilities
         /// </example>
         public async Task<List<T>?> Select<T>(string query, Func<MySqlDataReader, T> mapFunction)
         {
-            if (!query.Contains("SELECT")) // Dummy failsafe
+            if (string.IsNullOrWhiteSpace(query) || !query.Contains("SELECT")) // Dummy failsafe
             {
                 return null;
             }
@@ -73,7 +111,7 @@ namespace SelenicSparkApp_v2_WebAPI.Utilities
 
         public async Task<bool> Update(string query) // TODO
         {
-            if (!query.Contains("UPDATE"))
+            if (string.IsNullOrWhiteSpace(query) || !query.Contains("UPDATE"))
             {
                 return false;
             }
@@ -94,7 +132,7 @@ namespace SelenicSparkApp_v2_WebAPI.Utilities
 
         public async Task<bool> Delete(string query) // TODO
         {
-            if (!query.Contains("DELETE")) // Dummy failsafe
+            if (string.IsNullOrWhiteSpace(query) || !query.Contains("DELETE")) // Dummy failsafe
             {
                 return false;
             }
